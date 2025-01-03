4ecce5ce404efde5433c36afc3adab6b0490e199
maxigraf <maxigraf@maxigraf.de>
2025-01-03 11:31:31 +0100

    added num words

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        num_words = calculate_number_of_words(file_contents)
+        print(num_words)
+
+def calculate_number_of_words(text):
+    words = text.split()
+    return len(words)
 
 main()
