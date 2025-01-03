b0addf5893cbf54d768d0cce6ea145f8cc03a147
maxigraf <maxigraf@maxigraf.de>
2025-01-03 11:33:43 +0100

    added count chars

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        num_words = calculate_number_of_words(file_contents)
-        print(num_words)
+        char_counts = count_characters(file_contents)
+        print(char_counts)
 
 def calculate_number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    result = {}
+
+    for ch in text.lower():
+        if ch in result:
+            result[ch] += 1
+        else:
+            result[ch] = 1
+
+    return result
 
 main()
